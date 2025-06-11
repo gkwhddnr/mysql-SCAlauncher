8b39de80163cf766ca1f76300d70ec7dc4a4b725
WITHUS\WITHUS <gkwhddnr03@naver.com>
2025-06-11 23:20:59 +0900

    🎉 데이터베이스 연결 성공

---
+++ mysql-SCAlauncher/src/main/kotlin/kr/ac/kumoh/ce/s20221226/mysql_SCAlauncher/MysqlScAlauncherApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.ce.s20221226.mysql_SCAlauncher
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MysqlScAlauncherApplication
+
+fun main(args: Array<String>) {
+	runApplication<MysqlScAlauncherApplication>(*args)
+}
---
+++ mysql-SCAlauncher/src/test/kotlin/kr/ac/kumoh/ce/s20221226/mysql_SCAlauncher/MysqlScAlauncherApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20221226.mysql_SCAlauncher
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MysqlScAlauncherApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
